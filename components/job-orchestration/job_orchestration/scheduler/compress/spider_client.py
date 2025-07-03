3d66983f4c68813373e5bdad8c8f25e50453cc9c
sitaowang1998 <sitaowang1998@outlook.com>
2025-07-03 11:58:21 -0400

    Use msgpack to serialize/deserialize task  I/O values

--- components/job-orchestration/job_orchestration/scheduler/compress/spider_client.py
+++ components/job-orchestration/job_orchestration/scheduler/compress/spider_client.py
@@ -1,3 +1,4 @@
+import msgpack
 import uuid
 
 from typing import List, Union
@@ -27,12 +28,12 @@ def submit_jobs(db_conn, db_cursor, client_id, task_params) -> Union[List[uuid.U
     try:
         db_cursor.executemany(_job_insert_query, [(job_id, client_id) for job_id in job_ids])
         db_cursor.executemany(_task_insert_query, [(task_ids[i], job_ids[i], "clp_compress", "ready", 0, 0) for i in enumerate(task_params)])
-        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 0, "int", task_param["job_id"]) for i, task_param in enumerate(task_params)])
-        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 1, "int", task_param["task_id"]) for i, task_param in enumerate(task_params)])
-        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", task_param["tag_ids"].join(';')) for i, task_param in enumerate(task_params)])
-        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", task_param["clp_io_config_json"]) for i, task_param in enumerate(task_params)])
-        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", task_param["paths_to_compression_json"]) for i, task_param in enumerate(task_params)])
-        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", task_param["clp_metadata_db_connection_config"]) for i, task_param in enumerate(task_params)])
+        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 0, "int", msgpack.packb(task_param["job_id"])) for i, task_param in enumerate(task_params)])
+        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 1, "int", msgpack.packb(task_param["task_id"])) for i, task_param in enumerate(task_params)])
+        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", msgpack.packb(task_param["tag_ids"])) for i, task_param in enumerate(task_params)])
+        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", msgpack.packb(task_param["clp_io_config_json"])) for i, task_param in enumerate(task_params)])
+        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", msgpack.packb(task_param["paths_to_compression_json"])) for i, task_param in enumerate(task_params)])
+        db_cursor.executemany(_task_insert_input_value_query, [(task_ids[i], 2, "string", msgpack.packb(task_param["clp_metadata_db_connection_config"])) for i, task_param in enumerate(task_params)])
         db_cursor.executemany(_task_insert_output_query, [(task_ids[i], 0, "string") for i in range(len(task_params))])
         db_cursor.executemany(_task_insert_input_task_query, [(job_ids[i], task_ids[i], 0) for i in range(len(task_params))])
         db_cursor.executemany(_task_insert_output_task_query, [(job_ids[i], task_ids[i], 0) for i in range(len(task_params))])
@@ -48,7 +49,8 @@ def poll_result(db_conn, db_cursor, job_id: uuid.UUID):
     :param db_conn:
     :param db_cursor:
     :param job_id:
-    :return: Job output values if the job is completed, None otherwise.
+    :return: Job output values if the job is completed. If parsing values fails, return an empty
+             list. None if the job is not found or not completed.
     """
     db_cursor.execute(_job_status_query, (job_id,))
     job_status = db_cursor.fetchone()
@@ -64,12 +66,16 @@ def poll_result(db_conn, db_cursor, job_id: uuid.UUID):
     db_cursor.execute(_job_output_tasks_query, (job_id,))
     output_tasks = db_cursor.fetchall()
 
-    output_values = []
-    for task in output_tasks:
-        task_id = task[0]
-        db_cursor.execute(_task_output_values_query, (task_id,))
-        values = [value[0] for value in db_cursor.fetchall()]
-        output_values.extend(values)
+    try:
+        output_values = []
+        for task in output_tasks:
+            task_id = task[0]
+            db_cursor.execute(_task_output_values_query, (task_id,))
+            values = [msgpack.unpackb(value[0]) for value in db_cursor.fetchall()]
+            output_values.extend(values)
+    except Exception as e:
+        db_conn.commit()
+        return []
 
     db_conn.commit()
     return output_values
