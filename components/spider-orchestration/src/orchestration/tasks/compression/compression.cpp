8ebf4030c96399c27676ef9c59cb817898c73e6d
sitaowang1998 <sitaowang1998@outlook.com>
2025-07-07 15:21:11 -0400

    Update compress task signature in cpp

--- components/spider-orchestration/src/orchestration/tasks/compression/compression.cpp
+++ components/spider-orchestration/src/orchestration/tasks/compression/compression.cpp
@@ -3,7 +3,16 @@
 #include <string>
 
 namespace orchestration::compression {
-auto compress(std::string const& input) -> std::string {
-    return input;
+auto compress(
+    int job_id,
+    int task_id,
+    std::vector<int> const &tag_ids,
+    std::string const &clp_io_config_json,
+    std::string const &paths_to_compression_json,
+    std::string const &clp_metadata_db_connection_config,
+    std::string const &clp_compression_task_path
+) -> std::string {
+    return "";
 }
+
 }  // namespace orchestration::compression
--- components/spider-orchestration/src/orchestration/tasks/compression/compression.hpp
+++ components/spider-orchestration/src/orchestration/tasks/compression/compression.hpp
@@ -2,9 +2,18 @@
 #define ORCHESTRATION_TASKS_COMPRESSION_COMPRESSION_HPP
 
 #include <string>
+#include <vector>
 
 namespace orchestration::compression {
-auto compress(std::string const& input) -> std::string;
-}  // namespace orchestration::compression
+auto compress(
+    int job_id,
+    int task_id,
+    std::vector<int> const &tag_ids,
+    std::string const &clp_io_config_json,
+    std::string const &paths_to_compression_json,
+    std::string const &clp_metadata_db_connection_config,
+    std::string const &clp_compression_task_path
+) -> std::string;
+} // namespace orchestration::compression
 
 #endif
